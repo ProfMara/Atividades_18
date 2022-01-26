d87a585e893cc73569533869988499b76deacdd4
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-01-26 13:51:01 -0300

    Add files via upload

---
+++ Atividade_1/estudante.js
@@ -0,0 +1,10 @@
+class Estudante {
+
+    constructor() {
+
+    }
+
+    exibir() {
+
+    }
+}
---
+++ Atividade_1/sketch.js
@@ -0,0 +1,11 @@
+var aluno, aluno2;
+
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+function draw() {
+    background(220);
+
+}
